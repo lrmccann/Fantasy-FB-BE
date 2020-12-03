6e3a1fea47be41e16cbe81a11ba837a898ad170a
Logan McCAnn <loganrmccann@gmail.com>
2020-12-02 20:55:37 -0600

    auth route

--- routes/routes.js
+++ routes/routes.js
@@ -5,61 +5,79 @@ const usersController = require("../controller/controller");
 const db = require('../model/index');
 var axios = require("axios").default;
 
+const createSessiontoken = () => {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
 
-router.get('/hello' , async (req , res) => {
-    await db.User
+
+router.get('/hello', async (req, res) => {
+  await db.User
     .find({})
-    .map(function(data) {
-        // return res.send(data)
-        return res.json(data)
+    .map(function (data) {
+      // return res.send(data)
+      return res.json(data)
     })
 
-    router.get('/seasons' , async (req, res) => {
-      var options = {
-        method: 'GET',
-        url: 'https://api.sportsdata.io/v3/nfl/scores/json/Players?key=12afde5143164914a73228616f79c12f',
-        headers: {
-          "Ocp-Apim-Subscription-Key" : "12afde5143164914a73228616f79c12f"
-        }
-      };
-      
-      axios.request(options).then(function (response) {
-        console.log(response)
-          return res.json(response.data)
-        //   console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
-})
+  router.get('/seasons', async (req, res) => {
+    var options = {
+      method: 'GET',
+      url: 'https://api.sportsdata.io/v3/nfl/scores/json/Players?key=12afde5143164914a73228616f79c12f',
+      headers: {
+        "Ocp-Apim-Subscription-Key": "12afde5143164914a73228616f79c12f"
+      }
+    };
 
-router.get('/gamesByWeek' , async (req, res) => {
-  var options = {
-    method: 'GET',
-    url: 'https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2020?key=12afde5143164914a73228616f79c12f',
-    headers: {
-      "Ocp-Apim-Subscription-Key" : "12afde5143164914a73228616f79c12f"
-    }
-  };
-  axios.request(options).then(function (response) {
-    console.log(response)
+    axios.request(options).then(function (response) {
+      console.log(response)
       return res.json(response.data)
-    //   console.log(response.data);
-  }).catch(function (error) {
+      //   console.log(response.data);
+    }).catch(function (error) {
       console.error(error);
-  });
+    });
+  })
+
+  router.get('/gamesByWeek', async (req, res) => {
+    var options = {
+      method: 'GET',
+      url: 'https://api.sportsdata.io/v3/nfl/scores/json/Schedules/2020?key=12afde5143164914a73228616f79c12f',
+      headers: {
+        "Ocp-Apim-Subscription-Key": "12afde5143164914a73228616f79c12f"
+      }
+    };
+    axios.request(options).then(function (response) {
+      console.log(response)
+      return res.json(response.data)
+      //   console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  })
+
+  router.get("/authenticate/:id1/:id2", async (req, res) => {
+    console.log(req.params.id1, "username")
+    var username = req.params.id1
+    console.log(req.params.id2, "password")
+    var password = req.params.id2
+    let account = await db.User
+      .findOne({ 'userData.userName': username });
+    console.log(account, "this is account")
+    if(password === account.password){
+      console.log("password did match")
+      const token = createSessiontoken();
+      console.log(token , "i am session token")
+      await db.User.findOneAndUpdate(
+        { 'userData.userName': req.params.id1 },
+        { 'userData.sessionToken': token },
+        { new: true }    //Set new option to true to return the document AFTER update was applied.
+      )
+        .then(result => res.json(result.userData))
+    } if(!account){
+      res.json("Incorrect username, try again")
+    }else{
+      res.json("Incorrect password, try again")
+    }
+  })
+
 })
 
-router.get("/authenticate/:id1/:id2" , async (req, res) => {
-  console.log(req.params.id1 , "username")
-  var username = req.params.id1
-  console.log(req.params.id2 , "password")
-  var password = req.params.id2
-  let account = await db.User
-  .findOne({'userData.userName' : username });
-  console.log(account , "this is account")
-  .find()
-})
-
-})
-
-    module.exports = router;
+module.exports = router;
