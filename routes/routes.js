ddf45cbe1f32d6aa59cf4d4b80d95bcab357235d
Logan McCAnn <loganrmccann@gmail.com>
2020-12-02 23:10:48 -0600

    route

--- routes/routes.js
+++ routes/routes.js
@@ -81,7 +81,7 @@ router.get('/hello', async (req, res) => {
         console.log(token , "this is session token")
         console.log(account._id, "i am zee account id")
         await db.User.findByIdAndUpdate(
-            account._id,
+            { _id :account._id},
           { 'userData.sessionToken': token },
           // { new: true },    //Set new option to true to return the document AFTER update was applied.
           function(err , docs){
