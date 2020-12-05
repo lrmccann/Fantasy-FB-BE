4e4de888204dd2b63c65a5ec3d52c255fc6f28cc
Logan McCAnn <loganrmccann@gmail.com>
2020-12-05 01:22:02 -0600

    server

--- routes/routes.js
+++ routes/routes.js
@@ -123,7 +123,7 @@ router.post('/authent' , async (req, res) => {
   // }
   console.log("create-func", req.body)
   console.log("userObj", req.body)
-  let account = await db.User.findOne({ 'userData.userName': req.body.username });
+  let account = await db.User.findOne({ 'userName': req.body.username });
   console.log("account", account)
   if (!account) {
     const creds = saltHash(req.body.password);
