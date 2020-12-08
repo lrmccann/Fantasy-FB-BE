8491ac1cc550d86aa6cbab7b5a86434b258708a6
Logan McCAnn <loganrmccann@gmail.com>
2020-12-07 23:34:37 -0600

    server

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(cors());
 // solution for CORS policy issues
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin",  "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin , Content-Type , Accept , Authorization , X-Requested-With");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,HEAD');
+    // res.header("Access-Control-Allow-Credentials", true);
     next();
   });
 
