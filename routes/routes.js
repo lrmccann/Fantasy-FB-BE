6427eb523d9880cfec3bb2db388218102e2e6a6e
Logan McCAnn <loganrmccann@gmail.com>
2020-12-07 21:24:01 -0600

    server

--- routes/routes.js
+++ routes/routes.js
@@ -121,9 +121,10 @@ router.post('/createAccount/:id1' , async (req, res) => {
   // }else{
   //   res.json("Username is already taken")
   // }
+  console.log(...req , "this is to decomp the object")
   console.log("req boooodddyyy", req.body)
   console.log("req paaarraaaammmsss", req.body)
-  console.log( "req reguulllaarr" ,req)
+  console.log( "req reguulllaarr"  ,req)
   const account = await db.User.findOne({ 'userData.userName': req.body.userName });
   console.log("account", account)
   if (!account) {
