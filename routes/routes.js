48d843889b37bf89ab72660f0c75cb1d9c287275
Logan McCAnn <loganrmccann@gmail.com>
2020-12-07 23:03:53 -0600

    server

--- routes/routes.js
+++ routes/routes.js
@@ -133,7 +133,8 @@ router.post('/createAccount' , async (req, res) => {
     req.body.sessionToken = token;
     console.log("session token being created" , token)
     console.log(req.body , "i am request body after the first if conditional")
-    await db.User.create(req.body)
+    const createAccountInDb =  await db.User.create(req.body)
+    console.log(createAccountInDb)
     .then(result => console.log(result , "i am the final result in json form"))
       .then(result => res.json(result))
       .catch(err => res.status(422).json(err));
